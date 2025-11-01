10e8aa46427241652e10f431bb151f5397d9386a
ghodawalaaman <ghodawalaaman@gmail.com>
2025-11-01 16:15:35 +0530

    first commit

---
+++ backend/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Pptdata(models.Model):
+    id = models.IntegerField(primary_key=True)
+    keyword = models.CharField(max_length=255)
+    object = models.TextField()  # This field type is a guess.
+    timestamp = models.TextField(blank=True, null=True)  # This field type is a guess.
+
+    class Meta:
+        managed = False
+        db_table = 'pptdata'
