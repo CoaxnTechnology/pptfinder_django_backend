34a76f600fb738b2537492f5567cc6f1ac9b4ecc
ghodawalaaman <ghodawalaaman@gmail.com>
2025-11-03 18:07:08 +0530

    removed id from Model

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Pptdata(models.Model):
-    id = models.IntegerField(primary_key=True, auto_created=True)
     keyword = models.CharField(max_length=255)
     object = models.TextField()  # This field type is a guess.
     timestamp = models.TextField(blank=True, null=True)  # This field type is a guess.
